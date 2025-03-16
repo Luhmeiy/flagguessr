44097f9ed08d351297b13bb4c7e9f22b151c928c
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2025-03-16 17:18:29 -0300

    feat(landing-page): add dynamic flag counter

---
+++ src/app/api/flagCount/route.ts
@@ -0,0 +1,22 @@
+import { connectDB } from "@/lib/db";
+import { Flag } from "@/models/Flag";
+
+export async function GET() {
+	try {
+		await connectDB();
+
+		const flags = await Flag.countDocuments();
+
+		return new Response(JSON.stringify(flags), {
+			headers: { "Content-Type": "application/json" },
+		});
+	} catch (error) {
+		return new Response(
+			JSON.stringify({ error: "Failed to fetch flags" }),
+			{
+				status: 500,
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+	}
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import SettingsMenu from "@/components/SettingsMenu";
 
-export default function Home() {
+export default async function Home() {
+	const response = await fetch(`${process.env.BACKEND_URL}/api/flagCount`);
+	const flagCount = (await response.json()) as number;
+	const roundedDownFlagCount = Math.floor(flagCount / 50) * 50;
+
 	return (
 		<div className="flex flex-col items-center justify-center flex-1 p-8 pb-20 gap-8">
 			<h1 className="text-4xl font-bold">FlagGuessr</h1>
 
 			<div className="flex flex-col items-center">
 				<p className="text-sm">Test your flag knowledge!</p>
-				<p className="text-sm font-semibold">🌍 200+ Flags</p>
+				<p className="text-sm font-semibold">
+					🌍 {roundedDownFlagCount}+ Flags
+				</p>
 			</div>
 
 			<SettingsMenu />
